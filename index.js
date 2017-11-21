f26a4e7fcbfd6610a9df8f085b65c2c61159c7f6
d-king27 <kingd8883@gmail.com>
2017-11-21 14:46:21 +0000

    first workinng draft complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const app = require('./server.js');
-let PORT = require('./config').PORT[process.env.NODE_ENV];
-if(!PORT){PORT = process.env.PORT}
+const app = require("./server.js");
+let PORT = require("./config").PORT[process.env.NODE_ENV];
+if(!PORT){PORT = process.env.PORT;}
 
 app.listen(PORT,  () => {
-  console.log(`listening on port ${PORT}`);
+    console.log(`listening on port ${PORT}`);
 });
--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-const express = require('express')
-const app = express()
-const config = require('./config');
-if(!process.env.NODE_ENV){process.env.NODE_ENV = 'dev'}
-const db = config.DB[process.env.NODE_ENV]
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors')
-const { Users, Comments,Topics,Articles } = require('./models/models');
-const topicsRouter = require('./routes/topics.js')
-const articlesRouter = require('./routes/articles.js')
-const usersRouter = require('./routes/users.js')
-const commentsRouter = require('./routes/comments.js')
+const express = require("express");
+const app = express();
+const config = require("./config");
+if(!process.env.NODE_ENV){process.env.NODE_ENV = "dev";}
+const db = config.DB[process.env.NODE_ENV];
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { Users, Comments,Topics,Articles } = require("./models/models");
+const topicsRouter = require("./routes/topics.js");
+const articlesRouter = require("./routes/articles.js");
+const usersRouter = require("./routes/users.js");
+const commentsRouter = require("./routes/comments.js");
 mongoose.Promise = Promise;
 
 
 mongoose.connect(db, {useMongoClient: true})
-.then(() => console.log('successfully connected to', db))
-.catch(err => console.log('connection failed', err));
+    .then(() => console.log("successfully connected to", db))
+    .catch(err => console.log("connection failed", err));
 
-app.use(bodyParser.json())
-app.use(cors())
-app.get('/api',(req,res)=>{
-    res.send('listening')
-    console.log('test complete')
-})
-app.use('/api/users', usersRouter)
-app.use('/api/topics', topicsRouter)
-app.use('/api/comments', commentsRouter)
-app.use('/api/articles', articlesRouter)
+app.use(bodyParser.json());
+app.use(cors());
+app.get("/api",(req,res)=>{
+    res.send("listening");
+    console.log("test complete");
+});
+app.use("/api/users", usersRouter);
+app.use("/api/topics", topicsRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/articles", articlesRouter);
 
-app.use('/*', (req, res, next) => {
+app.use("/*", (req, res, next) => {
     res.status(404);
-    res.send({msg: 'Page not found'});
-  })
+    res.send({msg: "Page not found"});
+});
 app.use((err, req, res, next) => {
-    if(err.type === 'CastError') {
-      res.status(404);
-      res.send({msg: 'Page not found'});
+    if(err.type === "CastError") {
+        res.status(404);
+        res.send({msg: "Page not found"});
     }
     else {
-      res.status(500);
-      res.send({msg: err});
+        res.status(500);
+        res.send({msg: err});
     }
-  });
+});
 
-module.exports = app
+module.exports = app;
